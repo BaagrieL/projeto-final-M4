fc32121f184dabca7a5b2ef40fec3bdf33b681a0
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-05-17 23:41:18 -0300

    :sparkles: feat: Adicionando arquivo principal

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import redesolidariaRouter from "./router/redesolidaria.routes.js";
+import localizacaoRoutes from "./router/localizacao.routes.js";
 import { logEvents, logger } from "./middlewares/logger.middlewares.js";
 import dotenv  from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
+
 const app = express();
+app.use(cors());
 const PORT = process.env.BACKEND_PORT || 3000
 
 app.use(logger);
 app.use(express.json());
 
+//
 app.use("/Redesolidaria", redesolidariaRouter);
+app.use("/localizacao", localizacaoRoutes);
 
 app.listen(PORT, () => {
   logEvents(`Servidor rodando na porta ${PORT}`, "listen.log");
