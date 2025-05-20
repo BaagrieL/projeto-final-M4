94cf4fde2f5cbe3c5ed629e2a54f7e14eed820e5
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-05-20 00:55:50 -0300

    :sparkles: feat: Ajustes finais no arquivo server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express from 'express';
 import redesolidariaRouter from "./router/redesolidaria.routes.js";
-import localizacaoRoutes from "./router/localizacao.routes.js";
+import localizacao from './router/localizacao.routes.js';
+import organizacao from './router/organizacao.routes.js';
 import { logEvents, logger } from "./middlewares/logger.middlewares.js";
 import dotenv  from "dotenv";
 import cors from 'cors';
@@ -9,14 +10,17 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.BACKEND_PORT || 3000
+const PORT =  process.env.PORT || 3000;
 
 app.use(logger);
 app.use(express.json());
 
 //
-app.use("/Redesolidaria", redesolidariaRouter);
-app.use("/localizacao", localizacaoRoutes);
+app.use("/redesolidaria", redesolidariaRouter);
+
+//
+app.use("/localizacao", localizacao);
+app.use("/organizacao", organizacao);
 
 app.listen(PORT, () => {
   logEvents(`Servidor rodando na porta ${PORT}`, "listen.log");
